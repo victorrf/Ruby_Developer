817acc34c740d8d9848142733171fd101ab96507
Victor Reis Ferreira <victorreis-1998@hotmail.com>
2025-01-12 01:10:45 -0300

    Add files via upload
    Primeiro desafio concluído.

---
+++ Desafios/biblioteca.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+
+puts "Parabéns, senhor #{nome} #{sobrenome} com idade de #{idade} anos, por se cadastrar em nossa biblioteca!"
