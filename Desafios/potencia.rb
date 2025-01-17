ef361bc597e2053dd0d77acd976fad18b215eed9
Victor Reis Ferreira <victorreis-1998@hotmail.com>
2025-01-16 23:57:36 -0300

    Update potencia.rb
    Escrevendo os nomes das variáveis em inglês.

--- Desafios/potencia.rb
+++ Desafios/potencia.rb
@@ -1,14 +1,14 @@
-potencia = []
+power = []
 
 count = 0
 
 while count < 3
     print "\n Digite um número: "
     num = gets.chomp.to_f
-    pot = num**3
-    potencia.push(pot)
+    expo = num**3
+    power.push(expo)
     count += 1
 end
 
 puts "\n As potências são:"
-puts potencia
+puts power
